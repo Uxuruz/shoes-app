1fec9d0e871abb261362da68cfe32e3e2bceb4d3
Uxuruz <decs1183@gmail.com>
2024-02-29 23:54:33 -0600

    Se modifico archivo de barril con la exportacion de ZapatoPreview

--- lib/widgets/custom_widgets.dart
+++ lib/widgets/custom_widgets.dart
@@ -1,2 +1,4 @@
+export 'package:shoes_app/widgets/zapato_card_preview.dart';
 export 'package:shoes_app/widgets/custom_appbar.dart';
+
 
---
+++ lib/widgets/zapato_card_preview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ZapatoPreview extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container (
+      child: const Text('Hola'),
+    );
+  }
+}
